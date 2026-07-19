a42c0cf872abe1f779f48d2002c337e12622cfdd
Luke Nathan Hayes <126555240+auraofintelligence@users.noreply.github.com>
2026-07-19 23:19:48 +1000

    Tune initial map centre

--- app/RealMap.tsx
+++ app/RealMap.tsx
@@ -28,7 +28,7 @@ const ISLAND_BOUNDS: [[number, number], [number, number]] = [
   [153.57, -27.3],
 ];
 
-const INITIAL_CENTRE: [number, number] = [153.48, -27.39];
+const INITIAL_CENTRE: [number, number] = [153.48, -27.43];
 const INITIAL_ZOOM = 10.55;
 
 const QUEENSLAND_IMAGERY =
